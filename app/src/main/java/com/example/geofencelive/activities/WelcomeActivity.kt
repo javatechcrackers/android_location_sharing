b2e5759b7e746f40ed08d55f898d92a80fc50ff0
ashutoshpandey03 <ashutosh.pandey03@nagarro.com>
2024-08-08 14:11:41 +0530

    completed all 3 Login welcome screens with their features..

---
+++ app/src/main/java/com/example/geofencelive/activities/WelcomeActivity.kt
@@ -0,0 +1,36 @@
+package com.example.geofencelive.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.geofencelive.R
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var nextButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        nextButton = findViewById<RelativeLayout>(R.id.btnNext).findViewById(R.id.btn)
+
+        nextButton.text = "Next"
+
+        nextButton.setOnClickListener{
+            val intent = Intent(this, CreateProfileActivity::class.java)
+            startActivity(intent)
+            finish();
+
+        }
+    }
+}
